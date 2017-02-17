5948490487ac6ab047985af6ead56b9cdbaf4b30
yogakurniawan <yoga.kwn@gmail.com>
2017-02-17 16:21:46 +0700

    fix unit test for ContentList component

--- app/components/ContentList/tests/index.test.js
+++ app/components/ContentList/tests/index.test.js
@@ -1,56 +1,52 @@
 import { shallow, mount } from 'enzyme';
 import React from 'react';
-import { IntlProvider } from 'react-intl';
 
-import RepoListItem from 'containers/RepoListItem';
-import List from 'components/List';
+import BrandTile from 'components/BrandTile';
+import List from 'components/BasicList';
 import LoadingIndicator from 'components/LoadingIndicator';
-import ReposList from '../index';
+import ContentList from '../index';
 
-describe('<ReposList />', () => {
+describe('<ContentList />', () => {
   it('should render the loading indicator when its loading', () => {
     const renderedComponent = shallow(
-      <ReposList loading />
+      <ContentList loading />
     );
     expect(renderedComponent.contains(<List component={LoadingIndicator} />)).toEqual(true);
   });
 
   it('should render an error if loading failed', () => {
     const renderedComponent = mount(
-      <IntlProvider locale="en">
-        <ReposList
-          loading={false}
-          error={{ message: 'Loading failed!' }}
-        />
-      </IntlProvider>
+      <ContentList
+        loading={false}
+        error={{ message: 'Loading failed!' }}
+      />
     );
     expect(renderedComponent.text()).toMatch(/Something went wrong/);
   });
 
-  it('should render the repositories if loading was successful', () => {
-    const repos = [{
-      owner: {
-        login: 'mxstbr',
-      },
-      html_url: 'https://github.com/react-boilerplate/react-boilerplate',
-      name: 'react-boilerplate',
-      open_issues_count: 20,
-      full_name: 'react-boilerplate/react-boilerplate',
+  it('should render the brands if loading was successful', () => {
+    const brands = [{
+      title: 'APPLE',
+      totalProducts: 31,
+    }, {
+      title: 'SAMSUNG',
+      totalProducts: 1028,
     }];
     const renderedComponent = shallow(
-      <ReposList
-        repos={repos}
+      <ContentList
+        component={BrandTile}
+        brands={brands}
         error={false}
       />
     );
 
-    expect(renderedComponent.contains(<List items={repos} component={RepoListItem} />)).toEqual(true);
+    expect(renderedComponent.contains(<List items={brands} component={BrandTile} />)).toEqual(true);
   });
 
   it('should not render anything if nothing interesting is provided', () => {
     const renderedComponent = shallow(
-      <ReposList
-        repos={false}
+      <ContentList
+        brands={false}
         error={false}
         loading={false}
       />
--- app/components/ProductTile/index.js
+++ app/components/ProductTile/index.js
@@ -5,25 +5,22 @@ import RoundedBox from 'components/RoundedBox';
 import H4 from './H4';
 import Img from './Img';
 
-class Element extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    const { imgUrl, description, title } = this.props;
-    return (
-      <div className="row center-xs">
-        <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4" style={{ maxWidth: '30rem' }}>
-          <RoundedBox className="row">
-            <div className="col-xs-3">
-              <Img src={imgUrl} />
-            </div>
-            <div className="col-xs-9">
-              <H4>{title}</H4>
-              <P>{description}</P>
-            </div>
-          </RoundedBox>
-        </div>
+function Element({ imgUrl, description, title }) { // eslint-disable-line react/prefer-stateless-function
+  return (
+    <div className="row center-xs">
+      <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4" style={{ maxWidth: '30rem' }}>
+        <RoundedBox className="row">
+          <div className="col-xs-3">
+            <Img src={imgUrl} />
+          </div>
+          <div className="col-xs-9">
+            <H4>{title}</H4>
+            <P>{description}</P>
+          </div>
+        </RoundedBox>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Element.propTypes = {
