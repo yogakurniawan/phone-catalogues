2f31c92524b8c434e9051990ca3ec88d7a01c4c5
Yoga Kurniawan <yoga.kwn@gmail.com>
2017-02-18 09:12:45 +0700

    fix unit test for Header component

--- app/components/Header/tests/index.test.js
+++ app/components/Header/tests/index.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Header from '../index';
+import FixedHeaderContainer from '../FixedHeaderContainer';
+import NavBar from '../NavBar';
 
 describe('<Header />', () => {
-  it('should render a div', () => {
+  it('should render a <FixedHeaderContainer />', () => {
     const renderedComponent = shallow(
       <Header />
     );
-    expect(renderedComponent.find('div').length).toEqual(1);
+    expect(renderedComponent.find(FixedHeaderContainer).length).toEqual(1);
+  });
+
+  it('should render a <NavBar />', () => {
+    const renderedComponent = shallow(
+      <Header />
+    );
+    expect(renderedComponent.find(NavBar).length).toEqual(1);
   });
 });
