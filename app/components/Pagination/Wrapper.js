9d6b27631eae64a215935121539be5b0e70b40f2
yogakurniawan <yoga.kwn@gmail.com>
2017-03-06 15:51:18 +0700

    fix : make pagination responsive across devices

--- app/components/Pagination/Wrapper.js
+++ app/components/Pagination/Wrapper.js
@@ -5,6 +5,9 @@ const Wrapper = styled.div`
     padding-left: 0;
   }
   li {
+    @media (max-width: 375px) {
+      margin: auto 2px;
+    }
     margin: auto 5px;
     padding-bottom: 20px;
     display: inline-block;
@@ -17,6 +20,14 @@ const Wrapper = styled.div`
       text-align: center;
       border-radius: 3px;
       padding: 8px 12px;
+      @media (max-width: 414px) {
+        padding: 6px 9px;
+        font-size: 0.85em;
+      }
+      @media (max-width: 375px) {
+        padding: 6px 8px;
+        font-size: 0.75em;
+      }
       transition: border 0.25s linear, color 0.25s linear, background-color 0.25s linear;
       color: #dd5555;
       box-shadow: 0 0 1px rgba(0, 0, 0, 0.2), 0 2px 0 rgba(0, 0, 0, 0.1);
