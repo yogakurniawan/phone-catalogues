c9cdb812d71cb975a752be399a9ccf2b37e6fb7f
yogakurniawan <yoga.kwn@gmail.com>
2017-03-29 15:57:17 +0700

    feature dev --- device detail and specification

--- app/components/ProductDetail/index.js
+++ app/components/ProductDetail/index.js
@@ -15,13 +15,14 @@ const HeaderBox = styled(Div) `
   
   padding-left: 10px;
   background-color: #fbebeb;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%23ee9797' fill-opacity='0.23' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%23ee9797' fill-opacity='0.15' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `;
 
 const ImgWrapper = styled.div`
   img {
     border-radius: 14px;
   }
+  margin: 0 auto;
   padding: 10px;
   width: 180px;
   border-radius: 23px;
@@ -39,11 +40,21 @@ const TitleNav = styled.div`
   }
 `;
 
-const RightDescription = styled.div`
-  svg {
-    color: #2c3e50;
-  }
-`;
+// const RightDescription = styled.div`
+//   svg {
+//     color: #2c3e50;
+//   }
+// `;
+
+// const LeftIcon = styled.div`
+//   display: table-cell;
+//   margin-right: 4px;
+// `;
+
+// const RightLabel = styled.div`
+//   display: table-cell;
+//   padding-top: 4px;
+// `;
 
 function Element({ detail, onClick }) { // eslint-disable-line react/prefer-stateless-function
   return (
@@ -53,30 +64,10 @@ function Element({ detail, onClick }) { // eslint-disable-line react/prefer-stat
         <span><H2>{detail.name}</H2></span>
       </TitleNav>
       <div className="row">
-        <div className="col-xs-4">
+        <div className="col-xs-12">
           <ImgWrapper>
             <Img src={detail.imageurl} />
           </ImgWrapper>
-        </div>
-        <div className="col-xs-8 start-xs">
-          <RightDescription>
-            <div className="row">
-              <div style={{ display: 'table-cell', marginRight: '4px' }}>
-                <MdSdStorage size={16} />
-              </div>
-              <div style={{ display: 'table-cell', paddingTop: '4px' }}>
-                <P>{detail.internal}</P>
-              </div>
-            </div>
-            <div className="row">
-              <div style={{ display: 'table-cell', marginRight: '4px' }}>
-                <MdPhoneAndroid size={16} />
-              </div>
-              <div style={{ display: 'table-cell', paddingTop: '4px' }}>
-                <P>{`${detail.dimensions}, ${detail.weight}`}</P>
-              </div>
-            </div>
-          </RightDescription>
         </div>
       </div>
 
