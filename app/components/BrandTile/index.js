274c716363c127f59f42e725fda83c54c4df0cfb
yogakurniawan <yoga.kwn@gmail.com>
2017-04-12 10:34:55 +0700

    fix : brand title color issue

--- app/components/BrandTile/index.js
+++ app/components/BrandTile/index.js
@@ -5,11 +5,9 @@ import H3 from './H3';
 import H4 from './H4';
 import RoundedBox from './RoundedBox';
 
-const Title = styled.span`
+const BrandTitle = styled(H3)`
   @media (max-width: 480px) {
-    h3 {
-      font-size: 1em};
-    }
+    font-size: 1em;
   }
 `;
 
@@ -20,7 +18,7 @@ class Component extends React.Component { // eslint-disable-line react/prefer-st
       <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4">
         <RoundedBox className="row" to={`/devices/${encodeURIComponent(item.title)}?page=1`}>
           <div className="col-xs-12">
-            <Title><H3>{item.title}</H3></Title>
+            <BrandTitle>{item.title}</BrandTitle>
             <H4>{item.totalProducts} devices</H4>
           </div>
         </RoundedBox>
