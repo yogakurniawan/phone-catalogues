ac06c44b609877543f9e64e1e389f2f571fe6cba
Yoga Kurniawan <yoga.kwn@gmail.com>
2017-04-15 13:55:27 +0700

    change port

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
-const port = argv.port || process.env.PORT || 3005;
+const port = argv.port || process.env.PORT || 3000;
 
 // Start your app.
 app.listen(port, host, (err) => {
