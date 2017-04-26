0f93bad852f093de0e6eff6512cae8ce08349eb5
yogakurniawan <yoga.kwn@gmail.com>
2017-04-26 09:50:42 +0700

    fix : pathname

--- app/app.js
+++ app/app.js
@@ -80,7 +80,7 @@ const rootRoute = {
 
 const render = (messages) => {
   history.listen(function (location) {
-    window.ga('send', 'pageview', location.pathname);
+    window.ga('send', 'pageview', window.location.pathname);
   });
   ReactDOM.render(
     <Provider store={store}>
