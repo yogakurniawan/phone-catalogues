fec84385916130aa1fdd99c485f3074a2ddff99d
Yoga Kurniawan <yoga.kwn@gmail.com>
2017-06-04 22:39:18 +0700

    change ip

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -16,5 +16,5 @@ export const SET_PRODUCT_BRAND = 'boilerplate/App/SET_PRODUCT_BRAND';
 export const SET_SELECTED_DEVICE = 'boilerplate/App/SET_SELECTED_DEVICE';
 export const LOAD_DEVICE_BY_NAME = 'boilerplate/App/LOAD_DEVICE_BY_NAME';
 export const GET_DEVICE_BY_NAME_ERROR = 'boilerplate/App/GET_DEVICE_BY_NAME_ERROR';
-export const BASE_API_URL = '//phonecatalogues.com/api';
+export const BASE_API_URL = '//52.221.206.121:9000/api';
 export const DEFAULT_LOCALE = 'en';
