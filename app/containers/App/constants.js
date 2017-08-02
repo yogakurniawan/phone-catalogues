0dad9318f54a2131a8fea8be1fe4f0262ce463ef
Yoga Kurniawan <yoga.kwn@gmail.com>
2017-08-02 21:05:56 +0700

    change api url

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -3,5 +3,5 @@ export const SET_SELECTED_DEVICE = 'boilerplate/App/SET_SELECTED_DEVICE';
 export const SET_SEARCH_QUERY = 'boilerplate/App/SET_SEARCH_QUERY';
 export const LOAD_DEVICE_BY_NAME = 'boilerplate/App/LOAD_DEVICE_BY_NAME';
 export const GET_DEVICE_BY_NAME_ERROR = 'boilerplate/App/GET_DEVICE_BY_NAME_ERROR';
-export const BASE_API_URL = '//13.228.182.12:9000/api';
+export const BASE_API_URL = '//phonecatalogues.com/api';
 export const DEFAULT_LOCALE = 'en';
